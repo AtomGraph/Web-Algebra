81039fb3b6935f67eed6ae4cc8ffaefb1cbfdc57
Tim McIver <tim@timmciver.com>
2025-01-16 21:47:44 -0500

    Update ResolveURI in accordance with ldg_operations.txt.

--- src/operations/resolve_uri.py
+++ src/operations/resolve_uri.py
@@ -1,12 +1,19 @@
 from urllib.parse import urljoin
 from operation import Operation
+import uuid
 
 class ResolveURI(Operation):
-    def execute(self, base: str, relative_uri: str) -> str:
+    def execute(self, base: str, relative_uri: str = '') -> str:
         """
         Resolve a relative URI against a base URI.
+        If the slug string is provided, then it is used as the relative path,
+        otherwise a random UUID value is used as the slug. The slug value will
+        be URL-encoded if necessary.
         :param base: The base URI.
         :param relative_uri: The relative URI to resolve.
         :return: The resolved absolute URI as a string.
         """
-        return str(urljoin(base, relative_uri))
+        if not relative_uri:
+            relative_uri = str(uuid.uuid4())
+
+        return str(urljoin(base, relative_uri)) + '/'
