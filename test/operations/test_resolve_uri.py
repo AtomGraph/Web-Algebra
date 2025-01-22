e7796125763ea5aab7ffe0a3c9291716116b416b
Tim McIver <tim@timmciver.com>
2025-01-22 18:49:01 -0500

    Add test for ResolveURI without slug.

--- test/operations/test_resolve_uri.py
+++ test/operations/test_resolve_uri.py
@@ -1,10 +1,20 @@
 import pytest
+import re
 from operations.resolve_uri import ResolveURI
 
 def test_resolve_uri_with_slug():
-    print("Testing ResolveURI")
     base = 'http://localhost:80'
     slug = 'foobar'
     expected = base + '/' + slug + '/'
     actual = ResolveURI().execute(base, slug)
     assert actual == expected
+
+def test_resolve_uri_without_slug():
+    base = 'http://localhost:80'
+    uuid_re = r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
+    uri = ResolveURI().execute(base)
+    uuids = re.findall(uuid_re, uri)
+
+    assert uri.startswith(base)
+    assert uri.endswith('/')
+    assert len(uuids) == 1
